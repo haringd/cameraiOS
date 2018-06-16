52eb71a3982e8c81f6e12507b77570762df4ade6
Student <student@Students-iMac.local>
2018-06-16 18:23:20 -0400

    need to use a real phone to perorme camara testing

--- Camera/ViewController.swift
+++ Camera/ViewController.swift
@@ -7,12 +7,38 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var previewView: UIView!
+    
+    var captureSession: AVCaptureSession?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var input: AVCaptureInput?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+
+        let captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        do {
+            input = try AVCaptureDeviceInput(device: captureDevice!)
+        } catch {
+            print(error)
+        }
+        
+        captureSession = AVCaptureSession()
+        captureSession?.addInput(input!)
+        
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+        videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        videoPreviewLayer?.frame = view.layer.bounds
+        previewView.layer.addSublayer(videoPreviewLayer!)
+        
+        captureSession?.startRunning()
     }
 
     override func didReceiveMemoryWarning() {
