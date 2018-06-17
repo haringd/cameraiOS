113aad0b584e7dfd1d9154d820a7a83328c3f16c
Student <student@Students-iMac.local>
2018-06-16 23:11:13 -0400

    switched to another tutorial because only camera view was shown but nothing could be done with it.

--- Camera/ViewController.swift
+++ Camera/ViewController.swift
@@ -7,38 +7,18 @@
 //
 
 import UIKit
-import AVFoundation
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var previewView: UIView!
     
-    var captureSession: AVCaptureSession?
-    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
-    var input: AVCaptureInput?
-    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
 
-        let captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
-        
-        do {
-            input = try AVCaptureDeviceInput(device: captureDevice!)
-        } catch {
-            print(error)
-        }
-        
-        captureSession = AVCaptureSession()
-        captureSession?.addInput(input!)
-        
-        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
-        videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
-        videoPreviewLayer?.frame = view.layer.bounds
-        previewView.layer.addSublayer(videoPreviewLayer!)
-        
-        captureSession?.startRunning()
     }
 
     override func didReceiveMemoryWarning() {
@@ -46,6 +26,47 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func openCameraButton(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera;
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    
+    
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view, typically from a nib.
+//
+//
+//        let captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
+//
+//        do {
+//            input = try AVCaptureDeviceInput(device: captureDevice!)
+//        } catch {
+//            print(error)
+//        }
+//
+//        captureSession = AVCaptureSession()
+//        captureSession?.addInput(input!)
+//
+//        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+//        videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+//        videoPreviewLayer?.frame = view.layer.bounds
+//        previewView.layer.addSublayer(videoPreviewLayer!)
+//
+//        captureSession?.startRunning()
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
 
 }
 
